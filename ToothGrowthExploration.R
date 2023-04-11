9f65392f18789d80f4b8b2438398b76039a8f190
Michael Nguyen <77893030+junyimn@users.noreply.github.com>
2023-04-11 20:09:08 +0100

    Add files via upload

---
+++ ToothGrowthExploration.R
@@ -0,0 +1,24 @@
+# Firstly I ensure the 'ToothGrowth' dataset is loaded 
+data("ToothGrowth")
+
+# Now I view the dataset in its raw form 
+View(ToothGrowth)
+
+# Now I want to filter the dataset where dose = 0.5 and assigned it to a variable 
+filtered_tg <- filter(ToothGrowth, dose==0.5)
+
+# Again I want to view the dataset to check the filtered version
+View(filtered_tg)
+
+# Next I wan to sort it by the length of tooth 
+arrange(filtered_tg, len)
+
+# HOWEVER, now I would like to archive the same result but with NESTED FUNCTIONS 
+arrange(filter(ToothGrowth, dose==0.5), len)
+
+# Finally, I would like to simplify this even more by using a PIPE
+filtered_toothgrowth <- ToothGrowth %>% 
+  filter(dose==0.5) %>% 
+  group_by(supp) %>% 
+  summarise(mean_len = mean(len,na.rm = T),.group='drop')
+  arrange(len)
